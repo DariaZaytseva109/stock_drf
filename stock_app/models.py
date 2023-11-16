cb18a386dff8468ee7c74d76de256ae001043a00
DariaZaytseva109 <dawazai@mail.ru>
2023-11-16 20:28:10 +0100

    created models

--- stock_app/models.py
+++ stock_app/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
+
 from django.db import models
 
-# Create your models here.
+
+class Store(models.Model):
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        verbose_name='Наименование'
+    )
+    address = models.CharField(
+        max_length=200,
+        verbose_name='Адрес'
+    )
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        verbose_name='Наименование'
+    )
+
+
+
+
